3bf8d368f85055d5a95627f8886a70b2edf1ab76
Scott CTR Tully <scott.ctr.tully@faa.gov>
2018-08-29 06:30:04 -0400

    Change

--- ansible/playbooks/files/modules/pptx_chart.py
+++ ansible/playbooks/files/modules/pptx_chart.py
@@ -11,37 +11,46 @@ class Chart:
     self.prs = Presentation(filename)
 
   def bar_chart(self,categories,series_name,series_values):
-    slide = self.prs.slides.add_slide(self.prs.slide_layouts[5])
-    chart_data = ChartData()
-    chart_data.categories = categories
-    chart_data.add_series(series_name,series_values)
-    x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
-    slide.shapes.add_chart(
-        XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data
-    )
+    try:
+      slide = self.prs.slides.add_slide(self.prs.slide_layouts[5])
+      chart_data = ChartData()
+      chart_data.categories = categories
+      chart_data.add_series(series_name,series_values)
+      x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
+      slide.shapes.add_chart(
+          XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data
+      )
+    except:
+      pass
 
   def pie_chart(self,categories,series_name,series_values):
-    slide = self.prs.slides.add_slide(self.prs.slide_layouts[5])
-    chart_data = ChartData()
-    chart_data.categories = categories
-    chart_data.add_series(series_name, series_values)
+    try:
+      slide = self.prs.slides.add_slide(self.prs.slide_layouts[5])
+      chart_data = ChartData()
+      chart_data.categories = categories
+      chart_data.add_series(series_name, series_values)
+  
+      chart = slide.shapes.add_chart(
+          XL_CHART_TYPE.PIE, x, y, cx, cy, chart_data
+      ).chart
 
-    chart = slide.shapes.add_chart(
-        XL_CHART_TYPE.PIE, x, y, cx, cy, chart_data
-    ).chart
+      chart.has_legend = True
+      chart.legend.position = XL_LEGEND_POSITION.BOTTOM
+      chart.legend.include_in_layout = False
 
-    chart.has_legend = True
-    chart.legend.position = XL_LEGEND_POSITION.BOTTOM
-    chart.legend.include_in_layout = False
-
-    chart.plots[0].has_data_labels = True
-    data_labels = chart.plots[0].data_labels
-    data_labels.number_format = '0%'
-    data_labels.position = XL_LABEL_POSITION.OUTSIDE_END
+      chart.plots[0].has_data_labels = True
+      data_labels = chart.plots[0].data_labels
+      data_labels.number_format = '0%'
+      data_labels.position = XL_LABEL_POSITION.OUTSIDE_END
+    except:
+      pass
 
   def save(self,filename):
-    self.prs.save(filename)
-
+    try:
+      self.prs.save(filename)
+    except:
+      pass
+ 
 def main():
   module = AnsibleModule(
     argument_spec=dict(
