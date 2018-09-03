35b0a8068ad6543808805497d58fee8bd35ec280
Scott Tully <scott.tully@gmail.com>
2018-09-03 12:20:15 -0400

    Update

--- ansible/playbooks/files/modules/pptx_chart.py
+++ ansible/playbooks/files/modules/pptx_chart.py
@@ -24,7 +24,7 @@ class FWChart:
       chart_data = ChartData()
       chart_data.categories = categories      
       chart_data.add_series(series_name,count_items)
-      x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
+      x, y, cx, cy = Inches(3), Inches(2), Inches(8), Inches(4.5)
       slide.shapes.add_chart(
           XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data
       )
