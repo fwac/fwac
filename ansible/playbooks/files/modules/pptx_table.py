11e09b4b12e37a00c943607098d2bc28a9e0d566
Scott CTR Tully <scott.ctr.tully@faa.gov>
2018-09-05 13:07:01 -0400

    Change

--- ansible/playbooks/files/modules/pptx_table.py
+++ ansible/playbooks/files/modules/pptx_table.py
@@ -27,7 +27,7 @@ def table_create(prs,position,title_text,table_header,table_data):
       x += 1
     
     for y in xrange(1, len(table_data)):
-      for x in xrange(0, len(cols):
+      for x in xrange(0, len(cols)):
         table.cell(y, x).text = data[x]
   except:
     print sys.exc_info()[0]
@@ -45,7 +45,6 @@ def main():
       supports_check_mode=False
   )
   try:
-
     table_data = module.params['table_data']
     title_text = module.params['title']
     filename = module.params['filename']
