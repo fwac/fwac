8e7244320e5a3d9940f7243d8bcc990543955b7e
Scott Tully <scott.tully@gmail.com>
2018-09-06 20:11:35 -0400

    Update

--- ansible/playbooks/files/modules/word_cloud.py
+++ ansible/playbooks/files/modules/word_cloud.py
@@ -16,7 +16,8 @@ def main():
       background_color = dict(required=False, default='None'),
       contour_color = dict(required=False, default='white'),
       mode = dict(required=False, default='RGBA'),
-      word_file = dict(required=True),
+      word_file = dict(required=False),
+      words = dict(required=False)
       ),
       supports_check_mode=False
   )
@@ -31,16 +32,18 @@ def main():
     mode = module.params['mode']
 
     # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-    d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+    # # d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
     # Read the whole text.
-    text = open(path.join(d, word_file)).read()    
+    # # text = open(path.join(d, word_file)).read()    
     
     #ansible_mask = np.array(Image.open(path.join(d, mask_file)))
     wc = WordCloud(background_color=None, colormap='PRGn', max_words=2000, mode=mode, height=height, width=width)
-    wc.generate(text)
+    # # wc.generate(text)
+    wc.generate(words)
     
-    wc.to_file(path.join(d, image_file))
+    # # wc.to_file(path.join(d, image_file))
+    wc.to_file(image_file)
     module.exit_json(changed=True)
   except:
     pass
