278e302e9abfe63d14fc75e5b8ad0ae62c8ab0bc
Scott Tully <scott.tully@gmail.com>
2018-09-08 17:15:14 -0400

    Update

--- ansible/playbooks/files/modules/pptx_shape.py
+++ ansible/playbooks/files/modules/pptx_shape.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 import sys
 from pptx import Presentation
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.util import Inches
 
 def bullet_size(data, size):
     for i in xrange(0, len(data), size):
@@ -20,8 +22,35 @@ def main():
     filename = module.params['filename']
     count = module.params['count']
     prs = Presentation(filename)
-    
     bullets = module.params['bullets']
+    
+
+
+    for shape in bullet_size(shapes,count):
+        title_only_slide_layout = prs.slide_layouts[5]
+        slide = prs.slides.add_slide(title_only_slide_layout)
+        shapes = slide.shapes
+
+        shapes.title.text = 'Adding an AutoShape'
+
+        left = Inches(0.93)  # 0.93" centers this overall set of shapes
+        top = Inches(3.0)
+        width = Inches(1.75)
+        height = Inches(1.0)
+
+        shape = shapes.add_shape(MSO_SHAPE.PENTAGON, left, top, width, height)
+        shape.text = 'Step 1'
+
+        left = left + width - Inches(0.4)
+        width = Inches(2.0)  # chevrons need more width for visual balance
+
+        for n in range(2, 6):
+            shape = shapes.add_shape(MSO_SHAPE.CHEVRON, left, top, width, height)
+            shape.text = 'Step %d' % n
+            left = left + width - Inches(0.4)
+
+
+        
     for data in bullet_size(bullets,count):
       bullet_slide_layout = prs.slide_layouts[1]
       slide = prs.slides.add_slide(bullet_slide_layout)
