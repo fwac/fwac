36c00d9b366af3b0f762d16067e73fa2ffcc1913
Scott Tully <scott.tully@gmail.com>
2018-10-09 21:41:15 -0400

    Update

--- meeting02/bin/cron.sh
+++ meeting02/bin/cron.sh
@@ -2,4 +2,6 @@
 if ! ss -ant | grep 5050
 then
   /usr/bin/pypi-server --disable-fallback -i 127.0.0.1 -p 5050 -v --log-file /var/log/pypi.log /opt/python/packages &
+  sleep 5
+  /usr/bin/curl https://fwac.local &>/dev/null
 fi
