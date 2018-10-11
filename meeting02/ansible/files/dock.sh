51842133a6bb9cfff3a2f85fe6495a55a911893e
Scott Tully <scott.tully@gmail.com>
2018-10-10 20:36:24 -0400

    Update

--- meeting02/ansible/files/dock.sh
+++ meeting02/ansible/files/dock.sh
@@ -9,7 +9,7 @@ fi
 if test $NAME
 then
   echo
-  if docker run -d -it -v /opt/fwac:/opt/fwac -e NAME=$NAME --hostname $NAME --add-host="fwac:$IP" --add-host="pypi.local:$IP" --name=$NAME $TAG &> /tmp/$NAME
+  if docker run -d -it -v /opt/fwac:/opt/fwac -e NAME=$NAME --hostname $NAME --dns=10.0.0.1 --add-host="fwac:$IP" --add-host="pypi.local:$IP" --name=$NAME $TAG &> /tmp/$NAME
   then
     echo "Created new container named $NAME"
   elif docker start $NAME &>> /tmp/$NAME
@@ -20,7 +20,7 @@ then
   fi
   sleep 2
   echo
-  if ! docker exec -it $NAME /bin/bash 
+  if ! docker exec -it $NAME bash -c 'mkdir $HOSTNAME 2>/dev/null; cd $HOSTNAME; exec "${SHELL:-sh}"'
   then
     echo "I didn't make it into the container :("
     echo "You're on your own now"
